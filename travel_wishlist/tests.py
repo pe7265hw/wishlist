c9bb6b580d4b50a21bc353bed2b012d01cd44846
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-11-11 13:06:18 -0600

    Added tests, one with visited page failed probably due to issues with displaying information on visited page

--- travel_wishlist/tests.py
+++ travel_wishlist/tests.py
@@ -3,8 +3,96 @@ from django.urls import reverse
 
 from .models import Place
 
+class TestHomePage(TestCase):
+
+    def test_home_page_shows_empty_list_message_for_empty_database(self):
+        home_page_url = reverse('place_list')
+        response = self.client.get(home_page_url)
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertContains(response, 'You have no places in your wishlist.')
 
 
 
+class TestWishList(TestCase):
+    
+    fixtures = ['test_places']
+
+    def test_wishlist_contains_not_visited_places(self):
+        response = self.client.get(reverse('place_list'))
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        self.assertContains(response, 'Tokyo')
+        self.assertContains(response, 'New York')
+        self.assertNotContains(response, 'San Francisco')
+        self.assertNotContains(response, 'Moab')
 
 
+
+class TestVisitedPage(TestCase):
+
+    def test_visited_page_shows_empty_list_message_for_empty_database(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+        self.assertContains(response, 'You have not visited any places yet')
+
+
+
+class TestWishList(TestCase):
+    
+    fixtures = ['test_places']
+
+    def test_visited_list_shows_visited_places(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+
+        self.assertNotContains(response, 'Tokyo')
+        self.assertNotContains(response, 'New York')
+        self.assertContains(response, 'San Francisco')
+        self.assertContains(response, 'Moab')
+
+
+class TestAddNewTestPlace(TestCase):
+    def test_add_new_unvisited_place(self):
+        add_place_url = reverse('place_list')
+        new_place_data = {'name': 'Tokyo', 'visited': False}
+
+        response = self.client.post(add_place_url, new_place_data, follow=True)
+        # follow=True means when POST request is made the data is saved then redirects to reload homepage
+        # and another request is made as a result of the first request, follow=True causes the redirect
+        # to be followed which doesn't happen by default
+
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        response_places = response.context['places']
+        # context can be used to check the data that was used to create the template, a template combines
+        # data and html together, context is a dictionary, that can be used to check whatever the view is
+        # sending and combining with the template
+
+        self.assertEqual(1, len(response_places)) # check for only one place
+
+        tokyo_from_response = response_places[0]
+
+        tokyo_from_database = Place.objects.get(name='Tokyo', visited=False)
+
+        self.assertEqual(tokyo_from_database, tokyo_from_response)
+
+class TestVisitedPlace(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_visited_places(self):
+        visited_place_url = reverse('place_was_visited', args=(2, ))
+        response = self.client.post(visited_place_url, follow=True)
+
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        self.assertNotContains(response, 'New York')
+        self.assertContains(response, 'Tokyo')
+
+        new_york = Place.objects.get(pk=2)
+        self.assertTrue(new_york.visited)
+
+    def test_non_existant_place(self):
+        visit_nonexistant_place_url = reverse('place_was_visited', args=(1234566, ))
+        response = self.client.post(visit_nonexistant_place_url, follow=True)
+        self.assertEqual(404, response.status_code)
